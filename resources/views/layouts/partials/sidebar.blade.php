7e1867f2da056c571ef005edf250c8d5d8bb1899
ashley23458 <ashleybooth1997@gmail.com>
2020-02-13 19:50:13 +0000

    Add task index function

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -20,7 +20,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="{{route('task.index')}}">
                     <i class="material-icons dashboard-icons">list</i> <span class="text">Tasks</span>
                 </a>
             </li>
