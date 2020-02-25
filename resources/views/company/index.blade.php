edffe60cda543d2ce6345a107e3218d7995427c7
ashley23458 <ashleybooth1997@gmail.com>
2020-02-25 14:02:33 +0000

    Hide company buttons from users that aren't owners

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -27,13 +27,15 @@
 							<td><a href="#" class="alert-link">{{ $company->name }}</a></td>
 							<td>{{ $company->owner->name }}</td>
 							<td>
-								<a href="{{ route('company_invite', $company->id) }}" class="btn btn-secondary create-new" role="button"><i class="fas fa-envelope-open-text"></i> Send invitation</a>
 								@if ($company->id == Auth::user()->company_id)
 								    <a href="" class="btn btn-success create-new" role="button"><i class="fas fa-check"></i> Current default company</a>
 								@else
 								    <a href="{{ route('set_default_company', $company->id) }}" class="btn btn-secondary create-new" role="button"><i class="fas fa-pencil-alt"></i> Set default company</a>
 								@endif
-								<a href="{{ route('company.edit', $company->id) }}" class="btn btn-secondary create-new" role="button"><i class="fas fa-pencil-alt"></i> Edit</a>
+								@if ($company->user_id == Auth::user()->id)
+								    <a href="{{ route('company_invite', $company->id) }}" class="btn btn-secondary create-new" role="button"><i class="fas fa-envelope-open-text"></i> Send invitation</a>
+								    <a href="{{ route('company.edit', $company->id) }}" class="btn btn-secondary create-new" role="button"><i class="fas fa-pencil-alt"></i> Edit</a>
+								@endif
 							</td>
 						</tr>
 					@endforeach
