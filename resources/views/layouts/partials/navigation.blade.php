d058f88f91cf978c597dd8c125a26ce9449b4b64
ashley23458 <ashleybooth1997@gmail.com>
2020-02-28 10:41:10 +0000

    Add company employees page

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -10,7 +10,6 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="">test<span class="sr-only">test</span></a>
             </li>
         </ul>
         @auth
